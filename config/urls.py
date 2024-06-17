eb62b8ccd8c3cd3ea5074d447e3d18ec5485e511
hyewwon <rlagp424@gmail.com>
2024-06-17 23:21:37 +0900

    feat : swagger

--- config/urls.py
+++ config/urls.py
@@ -4,7 +4,17 @@ from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
 
+from drf_spectacular.views import SpectacularJSONAPIView
+from drf_spectacular.views import SpectacularRedocView
+from drf_spectacular.views import SpectacularSwaggerView
+from drf_spectacular.views import SpectacularYAMLAPIView
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+
+    path("docs/json/", SpectacularJSONAPIView.as_view(), name="schema-json"),
+    path("docs/yaml/", SpectacularYAMLAPIView.as_view(), name="swagger-yaml"),
+    path("docs/swagger/", SpectacularSwaggerView.as_view(url_name="schema-json"), name="swagger-ui",),
+    path("docs/redoc/", SpectacularRedocView.as_view(url_name="schema-json"), name="redoc",),
 
 ]+ static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
