fb0285190ad92a4dbdbfec85ca8055b9d08a09f1
hyewwon <rlagp424@gmail.com>
2024-08-19 10:13:02 +0900

    feat : create function to get user info

--- api/utils.py
+++ api/utils.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.models import User
 from rest_framework_simplejwt.state import token_backend
+import requests
 
 def getUser(token:str):
     try:
@@ -10,3 +11,22 @@ def getUser(token:str):
         user = None
 
     return user
+
+
+def getGoogleUserInfo(token):
+    '''
+        구글 토큰으로 유저 정보 조회
+    '''
+    user_data = {}
+    user_info_req = requests.get(f"https://www.googleapis.com/oauth2/v3/userinfo?access_token={token}")
+
+    if not user_info_req.ok:
+        return user_data
+
+    else:
+        user_info = user_info_req.json()
+        user_data['email'] = user_info.get('email')
+        user_data['profile_image'] = user_info.get('picture')
+        return user_data
+    
+
