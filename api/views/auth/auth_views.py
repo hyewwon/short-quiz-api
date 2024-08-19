7f5d97e31c7d5754d6d671202ab849262e032604
hyewwon <rlagp424@gmail.com>
2024-08-19 10:11:43 +0900

    chore : oauth views

--- api/views/auth/auth_views.py
+++ api/views/auth/auth_views.py
@@ -11,8 +11,7 @@ from django.db import transaction
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger, InvalidPage
 from django.contrib.auth.models import User
 
-from api.serializers.auth_serializers import MyTokenObtainPairSerializer, MyTokenRefreshSerializer, RefreshTokenIDSerializer, RefreshTokenSerializer, OAuthLoginSerializer, UserSerializer
-import requests
+from api.serializers.auth_serializers import MyTokenRefreshSerializer, RefreshTokenIDSerializer, LogoutSerializer, OAuthLoginSerializer, UserSerializer
 
 
 class LoginView(GenericAPIView):
@@ -25,45 +24,11 @@ class LoginView(GenericAPIView):
     def post(self, request, *args, **kwargs):
         serializer = self.get_serializer(data=request.data)
         if serializer.is_valid(raise_exception=True):
-            access_token = serializer.validated_data["access_token"]
-
-            user_info_req = requests.get(f"https://www.googleapis.com/oauth2/v3/userinfo?access_token={access_token}")
-
-            if not user_info_req.ok:
-                    return Response({"message" : "잘못된 토큰 정보입니다."}, status=status.HTTP_400_BAD_REQUEST)
-
-            user_info = user_info_req.json()
-            email = user_info.get("email")
-            profile_image = user_info.get("picture", "")
-
-            try:
-                user = User.objects.get(username=email, email=email)
-            except:
-                user = User.objects.create(
-                    username = email,
-                    email = email
-                )
-                user.profile.profile_image = profile_image
-                user.save()
-            
-            token = MyTokenObtainPairSerializer.get_token(user)
-            refresh_token = str(token)
-            access_token = str(token.access_token)
-            outstandingToken = OutstandingToken.objects.get(token = refresh_token)
-            response = Response(
-                {   
-                    "user" : UserSerializer(user).data,
-                    "message":"로그인 되었습니다.",
-                    "jwt_token" :{
-                        "access_token" : access_token,
-                        "refresh_token_index_id": outstandingToken.id,
-                        "refresh_token_exp" : outstandingToken.expires_at.timestamp()
-                    }
-                },
-                status=status.HTTP_200_OK                
-            )
-            return response
+            data = serializer.save()
+            data['message'] = '로그인 되었습니다.'
+            return Response(data, status=status.HTTP_200_OK)
         
+        return Response({'message' : serializer.error}, status=status.HTTP_400_BAD_REQUEST)
 
 
 class ReissueTokenView(GenericAPIView):
@@ -75,34 +40,28 @@ class ReissueTokenView(GenericAPIView):
 
     def post(self, request, *args, **kwargs):
         serializer = self.get_serializer(data=request.data)
-        if serializer.is_valid(raise_exception = True):
-            refresh_token_index_id = serializer.validated_data["refresh_token_index_id"]
-            try:
-                refresh = OutstandingToken.objects.get(pk = refresh_token_index_id)
-            except:
-                return Response(
-                    {"message": "refresh token 정보 오류"}, status=status.HTTP_400_BAD_REQUEST
-                )
+        serializer.is_valid(raise_exception = True)
+        refresh = serializer.validated_data['refresh']
 
-            serializer = MyTokenRefreshSerializer(data={"refresh": refresh.token})
-            serializer.is_valid(raise_exception=True)
-            refresh_token = serializer.validated_data["refresh"]
-            access_token = serializer.validated_data["access"]
-            outstandingToken = OutstandingToken.objects.get(token=refresh_token)
-            response = Response(
-                {
-                    "user" : UserSerializer(refresh.user).data,
-                    "message": "refresh token reissue",
-                    "jwt token" : {
-                        "access_token" : access_token,
-                        "refresh_token_index_id" : outstandingToken.id,
-                        "refresh_token_exp" : outstandingToken.expires_at.timestamp()
-                    }
-                },
-                status=status.HTTP_200_OK
-            )
+        serializer = MyTokenRefreshSerializer(data={"refresh": refresh.token})
+        serializer.is_valid(raise_exception=True)
+        refresh_token = serializer.validated_data["refresh"]
+        access_token = serializer.validated_data["access"]
+        outstandingToken = OutstandingToken.objects.get(token=refresh_token)
+        response = Response(
+            {
+                "user" : UserSerializer(refresh.user).data,
+                "message": "refresh token reissue",
+                "jwt token" : {
+                    "access_token" : access_token,
+                    "refresh_token_index_id" : outstandingToken.id,
+                    "refresh_token_exp" : outstandingToken.expires_at.timestamp()
+                }
+            },
+            status=status.HTTP_200_OK
+        )
 
-            return response
+        return response
 
 
 class LogoutView(GenericAPIView):
@@ -115,16 +74,12 @@ class LogoutView(GenericAPIView):
     def post(self, request, *args, **kwargs):
         serializer = self.get_serializer(data=request.data)
         if serializer.is_valid(raise_exception = True):
-            refresh_token_index_id = serializer.validated_data["refresh_token_index_id"]
-            try:
-                refresh = OutstandingToken.objects.get(pk = refresh_token_index_id)
-            except:
-                return Response({"message":"refresh token does not exist"}, status=status.HTTP_400_BAD_REQUEST)
-
-            serializer = RefreshTokenSerializer(data={'refresh':refresh.token})
-            serializer.is_valid(raise_exception=True)
-            serializer.save()
-
-            return Response(status=status.HTTP_204_NO_CONTENT)
+            refresh = serializer.validated_data['refresh']
+            logout_serializer = LogoutSerializer(data={'refresh' : refresh.token})
+            logout_serializer.is_valid(raise_exception=True)
+            logout_serializer.save()
+            return Response({'message' : '로그아웃 되었습니다.'}, status=status.HTTP_200_OK)
+        
+        return Response({'message' : '로그아웃 실패. 관리자에게 문의해주세요.'}, status=status.HTTP_400_BAD_REQUEST)
 
     
